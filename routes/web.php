edbf279c7b40f5b7dddf72b8deefcbbfda9babb5
TariqAyman <tariq.ayman94@gmail.com>
2020-08-25 02:35:04 +0200

    change route for app

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('items', 'ItemsController@index')->name('items');
+Route::post('/items/search', 'ItemsController@search')->name('items.search');
+
+Route::get('cart', 'CartController@index')->name('cart');
+Route::get('empty-cart', 'CartController@empty_cart')->name('empty-cart');
+Route::get('add-to-cart/{id}', 'CartController@addToCart')->name('add-to-cart');
+Route::patch('update-cart', 'CartController@update')->name('update-cart');
+Route::delete('remove-from-cart', 'CartController@remove')->name('remove-from-cart');
+
+Route::resource('order','OrdersController')->only(['index','store']);
+
+Route::get('checkout/success', 'CheckOutController@success')->name('checkout.success');
+Route::get('checkout/fail', 'CheckOutController@fail')->name('checkout.fail');
